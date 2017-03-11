a821d053a4178e748b723d65c8ced5c8a290df3e
Lucas Dupin <dupin@google.com>
2017-03-11 14:38:43 -0800

    cleanup

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 BIN_FOLDER=~/.bin
 
 #Backup of old files
-for file in ~/.vim ~/.vimrc ~/.ctags ~/.tmux.conf
+for file in ~/.vim ~/.vimrc ~/.tmux.conf
 do
 	if [ -h file ]; then
 		echo "backup for: $file"
@@ -14,21 +14,4 @@ done
 #Linking
 ln -s `pwd`/vim ~/.vim
 ln -s `pwd`/vimrc ~/.vimrc
-ln -s `pwd`/ctags ~/.ctags
 ln -s `pwd`/tmux.conf ~/.tmux.conf
-ln -s `pwd`/gitconfig ~/.gitconfig
-
-#Installing daemon
-if [ -d $BIN_FOLDER ]; then
-	echo ".bin already exists"
-else
-	#Creating dir
-	mkdir $BIN_FOLDER
-	#exporting to path
-	echo "export PATH=$BIN_FOLDER:\$PATH" >> ~/.bash_profile
-
-	#sourcing
-	source ~/.bash_profile
-fi
-
-echo "Yep!"
